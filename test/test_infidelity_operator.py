6a8a4ec36f31d02167afa1460431773f530a061e
Filippo Vicentini <filippovicentini@gmail.com>
2023-02-03 12:15:58 +0100

    black

--- test/test_infidelity_operator.py
+++ test/test_infidelity_operator.py
@@ -3,19 +3,20 @@ from pytest import approx
 import netket as nk
 import jax.numpy as jnp
 import numpy as np
-import scipy 
+import scipy
 
 from netket_fidelity._infidelity_operator import InfidelityOperator
 from netket_fidelity.operator._1qubit_rotations import Rx
 from netket_fidelity._infidelity_exact import _infidelity_exact
 from netket_fidelity._finite_diff import central_diff_grad, same_derivatives
 
+
 def _setup():
 
     N = 4
     n_samples = 1e6
     n_discard_per_chain = 1e3
-    
+
     hi = nk.hilbert.Spin(0.5, N)
 
     U = Rx(hi, 0, 0.01)
@@ -25,8 +26,18 @@ def _setup():
 
     model = nk.models.RBM(alpha=1, param_dtype=complex)
 
-    vstate_old = nk.vqs.MCState(sampler=sampler, model=model, n_samples=n_samples, n_discard_per_chain=n_discard_per_chain)
-    vstate_new = nk.vqs.MCState(sampler=sampler, model=model, n_samples=n_samples, n_discard_per_chain=n_discard_per_chain)
+    vstate_old = nk.vqs.MCState(
+        sampler=sampler,
+        model=model,
+        n_samples=n_samples,
+        n_discard_per_chain=n_discard_per_chain,
+    )
+    vstate_new = nk.vqs.MCState(
+        sampler=sampler,
+        model=model,
+        n_samples=n_samples,
+        n_discard_per_chain=n_discard_per_chain,
+    )
 
     return vstate_old, vstate_new, U, U_dagger
 
@@ -42,18 +53,24 @@ def test_infidelity():
         return _infidelity_exact(unravel(params), vstate, U_sparse)
 
     I_exact = _infidelity_exact(
-        vstate_new.parameters, vstate_old, U_sparse, 
+        vstate_new.parameters,
+        vstate_old,
+        U_sparse,
     )
 
-    grad_exact = central_diff_grad(_infidelity_exact_fun, params, 1.0e-5, vstate_old, U_sparse)
+    grad_exact = central_diff_grad(
+        _infidelity_exact_fun, params, 1.0e-5, vstate_old, U_sparse
+    )
 
-    for b in [True, False]: 
+    for b in [True, False]:
 
-        I_op = InfidelityOperator(vstate_old, U=U, U_dagger=U_dagger, sampling_Uold = b, c = -0.5)
-        
+        I_op = InfidelityOperator(
+            vstate_old, U=U, U_dagger=U_dagger, sampling_Uold=b, c=-0.5
+        )
+
         I_stat1 = vstate_new.expect(I_op)
         I_stat, I_grad = vstate_new.expect_and_grad(I_op)
-        
+
         I1_mean = np.asarray(I_stat1.mean)
         I_mean = np.asarray(I_stat.mean)
         err = 5 * I_stat1.error_of_mean
@@ -61,24 +78,33 @@ def test_infidelity():
         np.testing.assert_allclose(I_exact.real, I1_mean.real, atol=err, rtol=err)
 
         assert I1_mean.real == approx(I_mean.real, abs=1e-5)
-        assert np.asarray(I_stat1.variance) == approx(np.asarray(I_stat.variance), abs=1e-5)
+        assert np.asarray(I_stat1.variance) == approx(
+            np.asarray(I_stat.variance), abs=1e-5
+        )
 
         I_grad, _ = nk.jax.tree_ravel(I_grad)
-        
+
         same_derivatives(I_grad, grad_exact, rel_eps=1e-1)
 
-
     I_exact = _infidelity_exact(
-        vstate_new.parameters, vstate_old, scipy.sparse.identity(U_sparse.shape[0]), 
+        vstate_new.parameters,
+        vstate_old,
+        scipy.sparse.identity(U_sparse.shape[0]),
     )
 
-    grad_exact = central_diff_grad(_infidelity_exact_fun, params, 1.0e-5, vstate_old, scipy.sparse.identity(U_sparse.shape[0]))
+    grad_exact = central_diff_grad(
+        _infidelity_exact_fun,
+        params,
+        1.0e-5,
+        vstate_old,
+        scipy.sparse.identity(U_sparse.shape[0]),
+    )
 
-    I_op = InfidelityOperator(vstate_old, c = -0.5)
-    
+    I_op = InfidelityOperator(vstate_old, c=-0.5)
+
     I_stat1 = vstate_new.expect(I_op)
     I_stat, I_grad = vstate_new.expect_and_grad(I_op)
-    
+
     I1_mean = np.asarray(I_stat1.mean)
     I_mean = np.asarray(I_stat.mean)
     err = 5 * I_stat1.error_of_mean
@@ -90,5 +116,4 @@ def test_infidelity():
 
     I_grad, _ = nk.jax.tree_ravel(I_grad)
 
-    same_derivatives(I_grad, grad_exact, rel_eps = 1e-1)
-
+    same_derivatives(I_grad, grad_exact, rel_eps=1e-1)
