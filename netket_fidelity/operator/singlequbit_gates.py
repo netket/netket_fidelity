24cb8bf316c54ebb3c6da5941428fe425f2ac7e5
Filippo Vicentini <filippovicentini@gmail.com>
2023-08-05 16:35:52 +0200

    adhere to DiscreteJaxOperator interface

--- netket_fidelity/operator/singlequbit_gates.py
+++ netket_fidelity/operator/singlequbit_gates.py
@@ -1,14 +1,17 @@
 from functools import partial
-import jax.numpy as jnp
+
 import numpy as np
+
 import jax
+import jax.numpy as jnp
+
 from jax.tree_util import register_pytree_node_class
-import netket as nk
-from netket.operator import DiscreteOperator
+
+from netket.operator import DiscreteJaxOperator
 
 
 @register_pytree_node_class
-class Rx(DiscreteOperator):
+class Rx(DiscreteJaxOperator):
     def __init__(self, hi, idx, angle):
         super().__init__(hi)
         self.idx = idx
@@ -21,9 +24,6 @@ class Rx(DiscreteOperator):
     @property
     def H(self):
         return Rx(self.hilbert, self.idx, -self.angle * 2)
-
-    def __hash__(self):
-        return hash(("Rx", self.idx))
 
     def __eq__(self, o):
         if isinstance(o, Rx):
@@ -38,6 +38,10 @@ class Rx(DiscreteOperator):
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         return cls(*aux_data)
+
+    @property
+    def max_conn_size(self) -> int:
+        return 2
 
     @jax.jit
     def get_conn_padded(self, x):
@@ -72,21 +76,8 @@ def get_conns_and_mels_Rx(sigma, idx, angle):
     return conns, mels
 
 
-@nk.vqs.get_local_kernel_arguments.dispatch
-def get_local_kernel_arguments(vstate: nk.vqs.MCState, op: Rx):
-    sigma = vstate.samples
-    conns, mels = get_conns_and_mels_Rx(
-        sigma.reshape(-1, vstate.hilbert.size), op.idx, op.angle
-    )
-
-    conns = conns.reshape((sigma.shape[0], sigma.shape[1], -1, vstate.hilbert.size))
-    mels = mels.reshape((sigma.shape[0], sigma.shape[1], -1))
-
-    return sigma, (conns, mels)
-
-
 @register_pytree_node_class
-class Ry(DiscreteOperator):
+class Ry(DiscreteJaxOperator):
     def __init__(self, hi, idx, angle):
         super().__init__(hi)
         self.idx = idx
@@ -100,8 +91,9 @@ class Ry(DiscreteOperator):
     def H(self):
         return Ry(self.hilbert, self.idx, -self.angle * 2)
 
-    def __hash__(self):
-        return hash(("Ry", self.idx))
+    @property
+    def max_conn_size(self) -> int:
+        return 2
 
     def __eq__(self, o):
         if isinstance(o, Ry):
@@ -150,21 +142,8 @@ def get_conns_and_mels_Ry(sigma, idx, angle):
     return conns, mels
 
 
-@nk.vqs.get_local_kernel_arguments.dispatch
-def get_local_kernel_arguments(vstate: nk.vqs.MCState, op: Ry):  # noqa: F811
-    sigma = vstate.samples
-    conns, mels = get_conns_and_mels_Ry(
-        sigma.reshape(-1, vstate.hilbert.size), op.idx, op.angle
-    )
-
-    conns = conns.reshape((sigma.shape[0], sigma.shape[1], -1, vstate.hilbert.size))
-    mels = mels.reshape((sigma.shape[0], sigma.shape[1], -1))
-
-    return sigma, (conns, mels)
-
-
 @register_pytree_node_class
-class Hadamard(DiscreteOperator):
+class Hadamard(DiscreteJaxOperator):
     def __init__(self, hi, idx):
         super().__init__(hi)
         self.idx = idx
@@ -176,9 +155,6 @@ class Hadamard(DiscreteOperator):
     @property
     def H(self):
         return Hadamard(self.hilbert, self.idx)
-
-    def __hash__(self):
-        return hash(("Hadamard", self.idx))
 
     def __eq__(self, o):
         if isinstance(o, Hadamard):
@@ -194,6 +170,10 @@ class Hadamard(DiscreteOperator):
     def tree_unflatten(cls, aux_data, children):
         return cls(*aux_data)
 
+    @property
+    def max_conn_size(self) -> int:
+        return 2
+
     @jax.jit
     def get_conn_padded(self, x):
         xr = x.reshape(-1, x.shape[-1])
@@ -202,6 +182,7 @@ class Hadamard(DiscreteOperator):
         mels = mels.reshape(x.shape[:-1] + mels.shape[-1:])
         return xp, mels
 
+    @jax.jit
     def get_conn_flattened(self, x, sections):
         xp, mels = self.get_conn_padded(x)
         sections[:] = np.arange(2, mels.size + 2, 2)
@@ -225,17 +206,3 @@ def get_conns_and_mels_Hadamard(sigma, idx):
     mels = mels.at[0].set(((-1) ** ((cons.at[0, idx].get() + 1) / 2)) / jnp.sqrt(2))
 
     return cons, mels
-
-
-@nk.vqs.get_local_kernel_arguments.dispatch
-def get_local_kernel_arguments(vstate: nk.vqs.MCState, op: Hadamard):  # noqa: F811
-    sigma = vstate.samples
-    conns, mels = get_conns_and_mels_Hadamard(
-        sigma.reshape(-1, vstate.hilbert.size),
-        op.idx,
-    )
-
-    conns = conns.reshape((sigma.shape[0], sigma.shape[1], -1, vstate.hilbert.size))
-    mels = mels.reshape((sigma.shape[0], sigma.shape[1], -1))
-
-    return sigma, (conns, mels)
