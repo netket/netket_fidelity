af396c5f271404957216d011f09a9cc48807f4a6
Filippo Vicentini <filippovicentini@gmail.com>
2023-08-05 16:36:01 +0200

    remove ising, use netket isingjax

--- netket_fidelity/operator/__init__.py
+++ netket_fidelity/operator/__init__.py
@@ -1,6 +1,7 @@
 from .singlequbit_gates import Rx, Ry, Hadamard
-from .ising import Ising
 
+# from .ising import Ising
+from netket.operator import IsingJax as Ising
 
 from netket.utils import _hide_submodules
 
