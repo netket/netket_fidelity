0ec3475e6da59ff05c151454b30eb4cf08c76a30
sinibaldi <alessandro.sinibaldi@epfl.ch>
2023-09-05 15:35:12 +0200

    improve test

--- netket_fidelity/Renyi2/exact.py
+++ netket_fidelity/Renyi2/exact.py
@@ -1,21 +1,17 @@
-from functools import partial
-
 import jax.numpy as jnp
-import jax
-import netket as nk
 import qutip
 import numpy as np
+from functools import partial 
+import jax 
 
-from netket.utils.dispatch import TrueT
 from netket.vqs import FullSumState, expect
 from netket.stats import Stats
 
 from .operator import Renyi2EntanglementEntropy
 
-
 @expect.dispatch
 def Renyi2(vstate: FullSumState, op: Renyi2EntanglementEntropy):
-    
+
     if op.hilbert != vstate.hilbert:
         raise TypeError("Hilbert spaces should match")
 
@@ -23,37 +19,35 @@ def Renyi2(vstate: FullSumState, op: Renyi2EntanglementEntropy):
         vstate._apply_fun,
         vstate.parameters,
         vstate.model_state,
-        vstate._all_states,   
-        op.subsys,
+        vstate._all_states,
+        op.subsystem,
     )
 
 
-
-#@partial(jax.jit, static_argnames=("afun"))
 def Renyi2_sampling_FullSumState(
     afun,
     params,
     model_state,
     sigma,
-    subsys, 
+    subsystem,
 ):
 
     N = sigma.shape[-1]
-    
+
     state = jnp.exp(afun({"params": params, **model_state}, sigma))
     state = state / jnp.sqrt(jnp.sum(jnp.abs(state) ** 2))
-    
+
     state_qutip = qutip.Qobj(np.array(state))
 
     state_qutip.dims = [[2] * N, [1] * N]
 
     mask = np.zeros(N, dtype=bool)
 
-    if(len(subsys) == mask.size or len(subsys) == 0):
+    if len(subsystem) == mask.size or len(subsystem) == 0:
         out = 0
 
     else:
-        mask[subsys] = True
+        mask[subsystem] = True
 
         rdm = state_qutip.ptrace(np.arange(N)[mask])
 
@@ -61,7 +55,7 @@ def Renyi2_sampling_FullSumState(
         out = np.log2(np.trace(np.linalg.matrix_power(rdm, n))) / (1 - n)
 
         out = np.absolute(out.real)
-        
+
     S2_stats = Stats(mean=out, error_of_mean=0.0, variance=0.0)
-    
+
     return S2_stats
--- netket_fidelity/Renyi2/operator.py
+++ netket_fidelity/Renyi2/operator.py
@@ -1,62 +1,59 @@
-from typing import Optional, Any
-from functools import partial
+from typing import Optional
 
 import jax.numpy as jnp
 import numpy as np
 
-import netket as nk
 from netket.operator import AbstractOperator
 from netket.utils.types import DType
-from netket.utils.dispatch import TrueT
 
 
 class Renyi2EntanglementEntropy(AbstractOperator):
     def __init__(
         self,
-        hilbert: None, 
-        subsys: jnp.array,
+        hilbert: None,
+        subsystem: jnp.array,
         *,
         dtype: Optional[DType] = None,
     ):
-        
+
         r"""
-    Operator computing the Rényi2 entanglement entropy of a state |ψ⟩ for a partition with subsystem A:
+        Operator computing the Rényi2 entanglement entropy of a state |ψ⟩ for a partition with subsystem A:
 
-    .. math::
+        .. math::
 
-        S_2 = -\log_2 Tr_A ρ
+            S_2 = -\log_2 Tr_A ρ
 
-    where ρ = |ψ⟩⟨ψ| is the density matrix of the system and Tr_A indicates the partial trace over the subsystem A. 
+        where ρ = |ψ⟩⟨ψ| is the density matrix of the system and Tr_A indicates the partial trace over the subsystem A.
 
-    The Monte Carlo estimator of S_2 is:
+        The Monte Carlo estimator of S_2 is:
 
-    ..math::
+        ..math::
 
-        S_2 = - \log \mathbb{E}_{Π(σ,η) Π(σ',η')}[ψ(σ,η') ψ(σ',η) / ψ(σ,η) ψ(σ',η')] 
-        
-    where σ \in A, η \in Ā and Π(σ, η) = |Ψ(σ,η)|^2 / ⟨ψ|ψ⟩. 
+            S_2 = - \log \mathbb{E}_{Π(σ,η) Π(σ',η')}[ψ(σ,η') ψ(σ',η) / ψ(σ,η) ψ(σ',η')]
 
-    Args:
-        hilbert: hilbert space of the system.
-        subsys: subsystem in the partition.
+        where σ \in A, η \in Ā and Π(σ, η) = |Ψ(σ,η)|^2 / ⟨ψ|ψ⟩.
 
-    Returns:
-        Rényi2 operator for which computing expected value.
+        Args:
+            hilbert: hilbert space of the system.
+            subsystem: list of the indices identifying the degrees of freedom in one subsystem of the full system.
+                All indices should be integers between 0 and hilbert.size
 
-    Example:
-    """
-        
+        Returns:
+            Rényi2 operator for which computing expected value.
+
+        Example:
+        """
+
         super().__init__(hilbert)
 
         self._dtype = dtype
-        self._subsys= np.sort(np.array(subsys))
-        self._sys = jnp.arange(hilbert.size)
+        self._subsystem = np.sort(np.array(subsystem))
 
-        if(
-            self._subsys.size > self._sys.size
-            or jnp.where(self._subsys < 0)[0].size > 0 
-            or jnp.where(self._subsys > self._sys[-1])[0].size > 0 
-        ): 
+        if (
+            self._subsystem.size > hilbert.size
+            or np.where(self._subsystem < 0)[0].size > 0
+            or np.where(self._subsystem > hilbert.size)[0].size > 0
+        ):
 
             print("Invalid partition")
 
@@ -65,17 +62,15 @@ class Renyi2EntanglementEntropy(AbstractOperator):
         return self._dtype
 
     @property
-    def subsys(self):
-        return self._subsys
-
-    @property
-    def sys(self):
-        return self._sys
+    def subsystem(self):
+        r"""
+        list of indices for the degrees of freedom in the subsytem
+        """
+        return self._subsystem
 
     @property
     def is_hermitian(self):
         return True
 
     def __repr__(self):
-        return f"Renyi2EntanglementEntropy(subsys={self.subsys})"
-
+        return f"Renyi2EntanglementEntropy(hilbert={self.hilbert}, subsys={self.subsystem})"
