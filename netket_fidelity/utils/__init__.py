71f4c5595843a2156583df6a249e9d3c48699de9
Filippo Vicentini <filippovicentini@gmail.com>
2023-11-28 08:24:22 +0100

    simplify logic of logpsi_U wrapper (#11)
    @alleSini99

--- netket_fidelity/utils/__init__.py
+++ netket_fidelity/utils/__init__.py
@@ -1,5 +1,5 @@
 from .expect import expect_2distr
-from .sampling_Ustate import _logpsi_U
+from .sampling_Ustate import make_logpsi_U_afun, _logpsi_U_fun
 
 from netket.utils import _hide_submodules
 
